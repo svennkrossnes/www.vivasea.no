02da03b6005ced24c8055a00cf7949d392d0214d
svennkrossnes <svenn.krossnes@gmail.com>
2024-12-12 11:00:55 +0100

    Changes to Home for the www.vivasea.no website

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 function Home() {
     return (
         <div className="home">
-            <h1>Welcome to Vivasea</h1>
+            <h1>Welcome to Vivasea2</h1>
             <p>Aquaculture and technology innovation.</p>
         </div>
     );

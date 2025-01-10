5421d7b236de725c6249d4d2778e663a753d0ca4
svennkrossnes <svenn.krossnes@gmail.com>
2025-01-10 08:22:52 +0000

    updated to 2026 the home file with buble underwater

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer() {
     return (
         <footer className="footer">
-            <p>© 2025 Vivasea. All Rights Reserved.</p>
+            <p>© 2026 Vivasea. All Rights Reserved.</p>
         </footer>
     );
 }
